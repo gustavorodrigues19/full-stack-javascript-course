79aaed8c105cc1561e161cdbc8b01e123006bf60
Gustavo Camargo Rodrigues <gustavocr.developer@gmail.com>
2022-08-26 20:21:50 +0100

    gulp sass

---
+++ gulp/css/gulpfile.js
@@ -0,0 +1,20 @@
+const { parallel } = require('gulp')
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+
+function transformCSS() {
+    return gulp.src('src/sass/index.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(uglifycss({ "uglyComments": true }))
+        .pipe(concat('estilo.min.css'))
+        .pipe(gulp.dest('build/css'))
+}
+
+function copyHtml() {
+    return gulp.src('src/index.html')
+                .pipe(gulp.dest('build'))
+}
+
+exports.default = parallel(transformCSS, copyHtml)
