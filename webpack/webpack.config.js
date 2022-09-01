feb0629b39985c3142d325ab023c37e2a84c9002
Gustavo Camargo Rodrigues <gustavocr.developer@gmail.com>
2022-09-01 19:11:24 +0100

    webpack module with webserver

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,18 +1,34 @@
+const modeDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modeDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    devServer: {
+        contentBase: "./public",
+        port: 9000,
     },
     plugins: [
         new MiniCssExtractPlugin({
             filename: "estilo.css",
         })
     ],
+    optimization: {
+        minimizer: [
+            new UglifyPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ],
+    },
     module: {
         rules: [{
             test: /\.s?[ac]ss$/,
@@ -22,6 +38,9 @@ module.exports = {
                 'css-loader', //interpreta @import, url()...
                 'sass-loader',
             ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
         }]
     }
 }
