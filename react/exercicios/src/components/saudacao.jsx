99e0cb737d698678277476cf9956fc399f21c3d4
Gustavo Camargo Rodrigues <gustavocr.developer@gmail.com>
2022-09-11 23:50:58 +0100

    react class component

--- react/exercicios/src/components/saudacao.jsx
+++ react/exercicios/src/components/saudacao.jsx
@@ -8,11 +8,18 @@ export default class Saudacao extends Component {
     }
 
     setTipo(e){
-        console.log(e.target.value)
+        this.setState({tipo: e.target.value})
     }
 
     setNome(e){
-      console.log(e.target.value)
+      this.setState({nome: e.target.value})
+    }
+  
+  constructor(props) {
+    super(props)
+    
+    this.setNome = this.setNome.bind(this)
+    this.setTipo = this.setTipo.bind(this)
     }
     
     render() {
@@ -23,8 +30,8 @@ export default class Saudacao extends Component {
           <div>
               <h1>{tipo} {nome}</h1>
               <hr />
-              <input type="text" placeholder='tipo' value={this.tipo} onChange={e => this.setTipo(e)} />
-              <input type="text" placeholder='nome' value={this.nome} onChange={e => this.setNome(e)}/>
+              <input type="text" placeholder='tipo' value={tipo} onChange={this.setTipo} />
+              <input type="text" placeholder='nome' value={nome} onChange={this.setNome}/>
         </div>
         )
   }
